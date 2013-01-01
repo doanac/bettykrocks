c25eebc6d6326a1ea41018c41e9845fb2f2c4189
Andy Doan <andy.doan@linaro.org>
2012-12-31 23:28:32 -0600

    add sidebar/submenu support

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,27 +10,20 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
 
-"""
-# Blogroll
-LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-"""
-
 THEME = os.path.join(os.path.dirname(os.path.abspath(__file__)), "theme")
 DEFAULT_PAGINATION = False
 PAGE_URL = '{slug}.html'
 PAGE_SAVE_AS = '{slug}.html'
 GENERATE_SOURCE = True
 
+andymenu = [
+    {'label': 'About Me', 'url': '/andy.html'},
+    {'label': 'Hire Me', 'url': '/hire-andy.html'},
+]
+
 MENU_ITEMS = [
     { 'label':'Home', 'url': '/' },
     { 'label':'Bethany', 'url': '/bethany.html' },
-    { 'label':'Andy', 'url': '/andy.html' },
+    { 'label':'Andy', 'url': '/andy.html', 'submenu': andymenu },
     { 'label':'About Us', 'url': '/about-us.html' },
 ]
